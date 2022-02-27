80df52fddca975395ffbb2d7471a873e8c43d8cf
chadia12 <80862580+chadia12@users.noreply.github.com>
2022-02-27 12:15:55 -0600

    2/27/2022

---
+++ W1d5/fnExps.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// module.exports ={double, times100, myMap};
+/**
+ * 
+ * @param {number} num - number.
+ * @returns {number} - number 2 time.
+ */
+function double(num) {
+    return num * 2;
+}
+
+
+
+/**
+ * 
+ * @param {number} num - number. 
+ * @returns {number} - number 100 times
+ */
+function times100(num){
+return num * 100;
+}
+
+/**
+ * 
+ * @param {array} arr - arry of numbers.
+ * @param {function} fnc -function call.
+ * @returns {array} -array.
+ */
+function myMap(arr ,fnc){
+let newarr = [];
+
+  for(let elem of arr){
+   newarr.push( fnc(elem));
+  }
+   return newarr;
+    //return arr.map(fnc);
+}
+let arr1 =[-10, 0, 10, 20];
+console.log(myMap(arr1, double));

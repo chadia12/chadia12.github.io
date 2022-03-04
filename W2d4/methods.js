3996c000078b1770f6f3599f7b67669a5f9a9ebd
chadia12 <80862580+chadia12@users.noreply.github.com>
2022-03-03 19:46:43 -0600

    3/3

--- W2d4/methods.js
+++ W2d4/methods.js
@@ -2,7 +2,7 @@
 /* eslint-disable */
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
-module.exports = { groupById, unique, filterRangeInPlace, filterRange, Calculator }; //add all of your function names here that you need for the node mocha tests
+// module.exports = { groupById, unique, filterRangeInPlace, filterRange, Calculator }; //add all of your function names here that you need for the node mocha tests
 
 let arr = [5, 3, 8, 1];
 console.log(filterRangeInPlace(arr, 1, 4));

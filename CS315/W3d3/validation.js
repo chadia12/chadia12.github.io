497cfe68ab6a88093f0f8c6f371a3fbc5fee0b3b
chadia12 <80862580+chadia12@users.noreply.github.com>
2022-04-14 22:48:41 -0500

    4/14

--- CS315/W3d3/validation.js
+++ CS315/W3d3/validation.js
@@ -1,25 +1,25 @@
 "use strict";
 /*eslint-disable*/
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-// (function () {
-//   'use strict'
+//Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
 
-//   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//   var forms = document.querySelectorAll('.needs-validation')
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
 
-//   // Loop over them and prevent submission
-//   Array.prototype.slice.call(forms)
-//     .forEach(function (form) {
-//       form.addEventListener('submit', function (event) {
-//         if (!form.checkValidity()) {
-//           event.preventDefault()
-//           event.stopPropagation()
-//         }
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
 
-//         form.classList.add('was-validated')
-//       }, false)
-//     })
-// })()
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
 
 window.addEventListener('load', function () {
   const urlParamas = new URLSearchParams(window.location.search);
@@ -28,17 +28,7 @@ window.addEventListener('load', function () {
   document.getElementsByName("validationCustom01")[0].value = urlParamas.get("validationCustom01");
   document.getElementsByName("validationCustom04")[0].value = urlParamas.get("validationCustom04");
   document.getElementsByName("validationCustom05")[0].value = urlParamas.get("validationCustom05");
-  // document.getElementsByName("inlineRadio1").value = urlParamas.get("inlineRadio1");
-  // document.getElementsByName("inlineRadio2").value = urlParamas.get("inlineRadio2");
-
-  // if(urlParamas.get("inlineRadioOptions").length == 0 )
-  // return;
-  // for (let eachOption of document.getElementsByName("inlineRadioOptions")[0].options) {
-  //   if (urlParamas.get("inlineRadioOptions").split(",").includes(eachOption))
-  //     eachOption.selected = true;
-  //   else
-  //     eachOption.selected = false;
-  // }
+  
 
 
   if (urlParamas.get('select').length == 0)
@@ -50,7 +40,7 @@ window.addEventListener('load', function () {
       eachOption.selected = false;
   }
   
-  // document.getElementsByName("invalidCheck2")[0].value = urlParamas.get("invalidCheck2");
-  // document.getElementsByName("invalidCheck3")[0].value = urlParamas.get("invalidCheck3");
+  document.getElementsByName("terms")[0].value = urlParamas.get("terms");
+  document.getElementsByName("terms2")[0].value = urlParamas.get("terms2");
 
 });

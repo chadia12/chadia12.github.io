f9a5689d33a195624e21207aecca557bda026296
chadia12 <80862580+chadia12@users.noreply.github.com>
2022-04-15 07:58:36 -0500

    4/14

--- CS315/W2d4/part1.js
+++ CS315/W2d4/part1.js
@@ -26,22 +26,26 @@ let node1 = {
     children: [node2, node3],
     value: null,
 };
+//  QUESTION 1 PART 2##############################################
 
-// function displayValue(nodeObj){
-//     if (Array.isArray(nodeObj.children)) {
-//         console.log(nodeObj.name+ ":" + nodeObj.value);
-//         for (let element of nodeObj.children) {
-//             displayInArray(element);
-//         }
 
-//     } else {
+function displayValue(nodeObj){
+    if (Array.isArray(nodeObj.children)) {
+        console.log(nodeObj.name+ ":" + nodeObj.value);
+        for (let element of nodeObj.children) {
+            displayInArray(element);
+        }
 
-//         console.log(nodeObj.name +":" + nodeObj.value);
+    } else {
 
-//     }
-// }
+        console.log(nodeObj.name +":" + nodeObj.value);
 
-// console.log(displayValue(node1));
+    }
+}
+
+console.log(displayValue(node1));
+
+//  QUESTION 2 PART 2 ##############################################
 
 
 function displayInArray(nodeObj){
@@ -60,7 +64,7 @@ function displayInArray(nodeObj){
 
 console.log(displayInArray(node1));
 
-
+//#################################################### PART 3 ##################################################
 
 let nodeObj = {
 
@@ -76,123 +80,138 @@ let nodeObj = {
 
 };
 
+//QUESTION 1 PART 3 ##############################################
 
+function printName(node) {
+    if (Array.isArray(node.children)) {
+        console.log(node.name);
+        for (let element of node.children) {
+            printName(element);
+        }
 
-// function printName(node) {
-//     if (Array.isArray(node.children)) {
-//         console.log(node.name);
-//         for (let element of node.children) {
-//             printName(element);
-//         }
+    } else {
 
-//     } else {
+        console.log(node.name);
 
-//         console.log(node.name);
-
-//     }
-
-// }
-// console.log(printName(nodeObj));
-
-// function findName(node, target){
-//    if(node.name == target){
-//        return true;
-//    }
-//    else if(Array.isArray(node.children)){
-//        for(let elem of node.children){
-//            return contains(elem, target)
-//        }
-//    }
-//    else{
-//        return false;
-//    }
-    
-// }
-// console.log(findName(nodeObj, "chadia"));
-
-
-Question4
-function TreeNode(name){
-    this.name = name;
-    this.descendents = [];
+    }
 
 }
-const abe = new TreeNode("Abe");
-const homer = new TreeNode("Homer");
-const bart = new TreeNode("Bart");
-const lisa = new TreeNode("Lisa");
-const maggie = new TreeNode("Maggie");
+console.log(printName(nodeObj));
 
-abe.descendents.push(homer);
-homer.descendents.push(bart, lisa, maggie);
- function ListNode(value){
-     this.value = value;
- }
 
-function generateList(root){
-    if(root.descendents==null || root.descendents.length==0)
-            return new ListNode(root.value);
-        let linkedList = new ListNode(root.value);
-        let list=linkedList;
-        for(let eachChild of root.descendents){
-            
-            list.next = generateList(eachChild);
-            list = list.next;
-            
-        }
-        return linkedList;
+
+//QUESTION 2 PART 3 ##############################################
+
+
+function findName(node, target){
+   if(node.name == target){
+       return true;
    }
-
-  console.log(JSON.stringify(generateList(abe)))
-
-  // let linkedList = generateList(abe);
-// let linkedList = {value:1,next:{value:2,next:{value:3,next:{value:4}}}}
-  function traverse(list){
-      if(list==null )
-        return;
+   else if(Array.isArray(node.children)){
+       for(let elem of node.children){
+           return contains(elem, target)
+       }
+   }
+   else{
+       return false;
+   }
     
-      console.log("here ",list.value);
-      list = list.next;
-      return traverse(list);
-      
+}
+console.log(findName(nodeObj, "chadia"));
+
+
+//QUESTION 4 PART 3 ##############################################
+function ListNode(value) {
+    this.name = value;
+    }
+    
+    
+    
+    function generateList(root) {
+    
+    if (root.children == null || root.children.length == 0)
+    return new ListNode(root.name);
+    let linkedList = new ListNode(root.name);
+    let list = linkedList;
+    for (let eachChild of root.children) {
+    list.next = generateList(eachChild);
+    list = list.next;
+    }
+    return linkedList;
+    }
+    
+
+
+//QUESTION 5 PART 3##############################################
+
+function findListNode(list, target){
+    let listCont = null;
+    if(list.next == null){
+        if(list.name == target){
+            listCont = list;
+        }
+    
+    else{
+        listCont = findListNode(list.next, target);
   }
+}
+return listCont
+}
 
-
-//question 5
-// function findListNode(list, target){
-//     let listCont = null;
-//     if(list.next == null){
-//         if(list.name == target){
-//             listCont = list;
-//         }
-    
-//     else{
-//         listCont = findListNode(list.next, target);
-//   }
-// }
-// return listCont
-// }
-
-// console.log(findListNode(abe, "lisa"));
-
-//Question 6
+console.log(findListNode(abe, "lisa"));
 
 
 
+//QUESTION 6 PART 3 ##############################################
+function treeModifier(tree, modifierFunc) {
+    if (tree.children == null) {
+        modifierFunc(tree);
+    } else {
+        modifierFunc(tree);
+        for (let element of tree.children) {
+            treeModifier(element, modifierFunc);
+        }
+    }
+    return tree;
+}
 
-//Question 7
+function allCaps(node) {
+    node.name = node.name.toUpperCase();
+    return node;
+}
+function addStars(node) {
+    node.name = "***" + node.name + "***";
+    return node;
+}
+function reverseNode(node) {
+    let newName = [];
+    let arrayName = node.name.split("");
+    for (let element of arrayName) {
+        newName.unshift(element);
+    }
+    node.name = newName.join("");
+    return node;
+}
 
-// function treeCollector(tree) {
-//     let arr =[];
-//     if(tree.children == null){
-//        arr = arr.concat(tree.name);
-//     }
-//     else{
-//         arr = arr.concat(tree.name);
-//         for(let sub of tree.children){
-//             arr = arr.concat(treeCollector(sub));
-//         }
-//     }
-//     return arr;
-// }
-// console.log(treeCollector(nodeObj));
+ console.log(treeModifier(Abe, allCaps))
+console.log(treeModifier(Abe, addStars))
+console.log(treeModifier(Abe, reverseNode))
+
+
+
+//QUESTION 7 PART 3 ##############################################
+
+function treeCollector(tree) {
+    let arr =[];
+    if(tree.children == null){
+       arr = arr.concat(tree.name);
+    }
+    else{
+        arr = arr.concat(tree.name);
+        for(let sub of tree.children){
+            arr = arr.concat(treeCollector(sub));
+        }
+    }
+    return arr;
+}
+console.log(treeCollector(nodeObj));

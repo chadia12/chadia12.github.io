e67369ab50b8cc94db6f2451443a9501bf771030
chadia12 <80862580+chadia12@users.noreply.github.com>
2022-05-09 20:44:23 -0500

    5/9

---
+++ CS445/lab7/question2.js
@@ -0,0 +1,18 @@
+"use strict";
+/**eslint-disable */
+
+Array.prototype.removeDuplicatesAsync= function(){
+let arr = [];
+for(let elem of this){
+    if(!arr.includes(elem))
+    arr.push(elem);
+}
+new Promise(function(resolve, reject){
+resolve(arr);
+}).then(result => console.log(result));
+}
+
+
+console.log("start");
+[4, 1, 5, 7, 2, 3, 1, 4, 6, 5, 2].removeDuplicatesAsync(); 
+console.log("end");

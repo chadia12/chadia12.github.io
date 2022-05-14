cc07f1ae3d3122b91360959c90fd43dd3858ebe2
chadia12 <80862580+chadia12@users.noreply.github.com>
2022-05-14 18:57:48 -0500

    5/14

--- CS445/project/musicproject/musicpage.js
+++ CS445/project/musicproject/musicpage.js
@@ -184,12 +184,14 @@ function searchMusic() {
             <td>${count}</td>
             <td>${element.title}</td>
             <td>${element.releaseDate}</td>
-            <td><button type="button" class="btn btn-dark text-center" id="addbtn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-plus-fill" viewBox="0 0 16 16">
+            <td><button type="button" class="btn btn-dark text-center addSong" data-add="${element.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-plus-fill" viewBox="0 0 16 16">
                 <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM8.5 6v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 1 0z"/>
               </svg></button></td>
         </tr>`
                 ++count;
                 allMusic.innerHTML += res;
+                addSongToPlayList()
+
             });
             searchSong.value = "";
         })

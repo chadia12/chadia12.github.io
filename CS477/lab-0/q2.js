c84a65bfb2641062f5abda121a7826c281158eef
chadia12 <80862580+chadia12@users.noreply.github.com>
2022-07-18 20:16:30 -0500

    jhkjh

---
+++ CS477/lab-0/q2.js
@@ -0,0 +1,66 @@
+"use strict";
+/* eslint-disable */
+let db = [
+    {id:1, fname: 'John', lname: 'Smith'},
+    {id:2, fname: 'Lucy', lname: 'Jark'},
+    {id:3, fname: 'Edward', lname: 'Capton'}
+];
+
+class Student {
+    constructor(id, firstname, lastname){
+        this.id = id;
+        this.fname = firstname;
+        this.lname = lastname;
+    }
+
+    save(){
+      let obj={}
+     obj.id = this.id;
+     obj.fname = this.fname;
+     obj.lname = this.lname;
+       db.push(obj);
+  
+    }
+
+    edit(){
+        let obj={}
+     obj.id = this.id;
+     obj.fname = this.fname;
+     obj.lname = this.lname;
+
+
+
+
+    }
+
+    static getById(id){
+let newId =db.filter(function(elem){
+return elem.id == id;
+});
+console.log(newId);
+    }
+
+    static getAll(){
+
+        console.log(db);
+    }
+
+
+   static  deleteById(id){
+let index = db.findIndex((elem) => elem.id ===id);
+if(index > -1){
+    db.splice(index,1);
+}
+else{
+    throw new Error("INDEX NOT FOUND");
+}
+    }
+
+}
+
+new Student(4, 'Tina', 'Xing').save(); //save to db
+
+new Student(4, 'Miss', 'Xing').edit() //edit studentId with id=4
+//Student.deleteById(4); //remove studentId=4 from db
+Student.getAll(); //return db;
+//Student.getById(1); //return {id:1, fname: 'John', lname: 'Smith'}

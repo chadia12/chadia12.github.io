d12b76ce8e820688cd744886f04dfb80705186d0
chadia12 <80862580+chadia12@users.noreply.github.com>
2022-07-25 20:59:18 -0500

    3

--- CS477/lab4/question1/app.js
+++ CS477/lab4/question1/app.js
@@ -16,11 +16,20 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.set('port', process.env.Port || 3000);
+app.set('port', process.env.Port || 3100);
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'user.html'));
 
-app.use('/loginpage', (req, res, next) => {
+});
+
+app.get('/product', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'product.html'));
+})
+
+app.post('/loginpage', (req, res, next) => {
     if (req.body.uname === 'chadia' && req.body.mail === 'cha@12.com' && req.body.psw === '123') {
         res.redirect('/product');
     }
@@ -32,13 +41,11 @@ app.use('/loginpage', (req, res, next) => {
 });
 
 
-app.use('/product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'product.html'));
-})
 
-app.use('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'user.html'));
 
+
+app.use((req, res, next) => {
+    res.status(404).send("URL doesn't exiist , try again");
 });
 
 app.listen(app.get('port'), () => console.log(`${app.get('port')}`));

d8f49a4d0b4a664702a4798773597f008187477f
chadia12 <80862580+chadia12@users.noreply.github.com>
2022-09-06 11:59:04 -0500

    chadia

--- CS477/project/server/controllers/userController.js
+++ CS477/project/server/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
 const Response = require('../models/responseobj');
 
+
 exports.getAllUsers = async (req, res, next) => {
  
     res.json(await User.find({$text: {$search : req.query.search}}));
